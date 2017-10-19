8739ef88134c9a42ad2a3d7a6dc19faf1eb145d0
rbardenet <remi.bardenet@gmail.com>
2017-10-19 12:22:09 +0200

    added test and demo notebook

---
+++ tests/test_hyperbolic.py
@@ -0,0 +1,26 @@
+import pytest
+import sys
+import os
+import glob
+sys.path.append("../hyperbolic")
+import hyperbolic as hype
+
+def test_Experiment():
+    """
+    make sure the main experiment runs smoothly and produces plots
+    """
+
+    # Run experiment
+    xp = hype.Experiment(alpha=2.0, expId="pytest")
+    xp.sampleWhiteNoise()
+    xp.performAWT()
+    xp.findZeros()
+    xp.plotResults(boolShow=0)
+
+    # Check wheter 3 plots have been created
+    pattern = "*_pytest_alpha*.pdf"
+    figNames = glob.glob(pattern)
+    os.popen("rm "+pattern)
+    print(figNames)
+    print(len(figNames))
+    assert(len(figNames)==3)
